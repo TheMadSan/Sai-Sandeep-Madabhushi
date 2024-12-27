2ecc5deb000f90a4a88c41da88efe0e0779fdc86
Sai_Sandeep_Madabhushi <ssm@uci.edu>
2024-12-26 22:38:51 -0800

    Initial Commit

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base:'/SSM-portfolio/',
+  base:'/sai-sandeep-madabhushi',
   plugins: [react()],
-  optimizeDeps: { exclude: ["fsevents"] },
-  server: {
-    hmr: {
-      overlay: false, // Disable HMR overlay to check if it's causing issues
-    },
-  },
+  
 });
